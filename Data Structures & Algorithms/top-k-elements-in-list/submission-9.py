8a7cc8a212924c75431e6c282e5f612b81cdc421
Jack <124582858+jackdoesjava@users.noreply.github.com>
2026-04-02 22:52:33 +0100

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.py
@@ -0,0 +1,9 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # put each val in hashmap
+        # then i want to find k most frequent numbers
+        seen = {}
+        for num in nums:
+            seen[num] = seen.get(num, 0) + 1
+
+        return sorted(seen, key=seen.get, reverse=True)[:k] # truncated to k elements
